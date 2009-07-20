846d2e55784660c81d01844a0e1ac6cb539f996d
beepscore <support@beepscore.com>
2009-07-19 17:04:33 -0700

    Initial commit.

---
+++ Classes/HelloPolyAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  HelloPolyAppDelegate.h
+//  HelloPoly
+//
+//  Created by Steve Baker on 7/19/09.
+//  Copyright Beepscore LLC 2009. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloPolyAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
---
+++ Classes/HelloPolyAppDelegate.m
@@ -0,0 +1,29 @@
+//
+//  HelloPolyAppDelegate.m
+//  HelloPoly
+//
+//  Created by Steve Baker on 7/19/09.
+//  Copyright Beepscore LLC 2009. All rights reserved.
+//
+
+#import "HelloPolyAppDelegate.h"
+
+@implementation HelloPolyAppDelegate
+
+@synthesize window;
+
+
+- (void)applicationDidFinishLaunching:(UIApplication *)application {    
+
+    // Override point for customization after application launch
+    [window makeKeyAndVisible];
+}
+
+
+- (void)dealloc {
+    [window release];
+    [super dealloc];
+}
+
+
+@end
