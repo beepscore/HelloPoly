aa31f0bcdee865a64941ea2c43382def2cde7f73
beepscore <support@beepscore.com>
2009-07-20 11:21:19 -0700

    Generate controller .h and .m files, put log statements in decrease, increase methods.
    Tested decrease, increase buttons in iphone simulator.
    Each button logs a message to Xcode Console as desired.

---
+++ Controller.m
@@ -0,0 +1,11 @@
+#import "Controller.h"
+
+@implementation Controller
+- (IBAction)decrease {
+    NSLog(@"controller class decrease method");
+}
+
+- (IBAction)increase {
+    NSLog(@"controller class increase method");    
+}
+@end
